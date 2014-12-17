d5fabe7b5f5cab8136be7ea80bc6408053a733aa
mikebocian <michbocian@gmail.com>
2014-12-17 16:32:32 +0100

    parsing dotProject

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(){
+﻿document.addEventListener('DOMContentLoaded', function(){
 
         chrome.runtime.sendMessage({method:'getJiraLogin'}, function(response){
           if(response.connected==false){
@@ -11,7 +11,87 @@ document.addEventListener('DOMContentLoaded', function(){
             return;
           }
 
+		  loadTime(function(data){
+			console.log(data);
+		  });
 
+		  
+		  function loadTime(callback) {
+	$.ajax({
+	   url: 'http://dotproject.psi.pl/index.php?m=projects&a=reports&project_id=399&report_type=userlogsaggrpertask&log_start_date=20141215&log_end_date=20141219&log_userfilter=0&do_report=submit#',
+	   data: {},
+	   success: function (data, status, jqXHR) {
+			var results = new Array();
+			var table = $($(data).find('.tbl'));
+			table.find('tr').each(function(tr){
+			var tds = $(this).find('td');
+				if(tds.length===6)
+				{
+				
+				if(isDevelopmentOrDocumentationTask(tds[2])){
+				
+					var username = getUniqueUserName($(tds[0]).html());
+					if(results[username] == undefined){
+						results[username] = new Array();
+					}
+					var date = getDate(tds[4]);
+					var entry = getEntry(tds[5]);
+					
+					if(results[username][date.getDay()-1]==undefined){
+
+					 results[username][date.getDay()-1] = entry }
+					 else{
+
+					  results[username][date.getDay()-1] += entry
+					 }
+				}}
+			});
+			
+			callback.call(this,results);
+			
+	   },
+	   error: function (data, status, error) {
+
+	   },
+	   dataType: 'html'
+	});
+	
+	function isDevelopmentOrDocumentationTask(td){
+		return /Development|Documentation/.test($(td).html());
+	}
+	
+	function getEntry(td){
+		return parseFloat( $(td).html());
+	}
+	
+	function getDate(td){
+		var test = $(td).html().split('/')
+		return new Date(test[2],test[1]-1,test[0])
+	}
+	
+	function getUniqueUserName(username){
+		switch(username){
+			case 'Bocian Michał':
+				return 'mbocian';
+			case 'Ćmil Michał':
+				return 'mcmil';
+			case 'Ostrowski Piotr':
+				return 'postrowski';
+			case 'Jaroszewicz Adrian':
+				return 'ajaroszewicz';
+			case 'Drąg Łukasz':
+				return 'ldrag';
+			case 'Becela Piotr':
+				return 'pbecela';
+			case 'Robakowski Przemko':
+				return 'probakowski';
+		}
+	}
+};
+		  
+		  
+		  
+		  
 
         $('#loader').html('<img src="img/ajax-loader.gif" />'); 
         $('#logsTable').remove();
@@ -45,8 +125,5 @@ document.addEventListener('DOMContentLoaded', function(){
         }
 
         });
-
-
-
     });
 } );
